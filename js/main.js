3cb314aac5b2028fe2bab0f406438975f2e7574f
Anton Stoychev <antitoxic@gmail.com>
2013-08-04 19:33:04 +0100

    image url fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('<img/>').attr('src', '/img/bg.png').load(function() {
+    $('<img/>').attr('src', 'img/bg.png').load(function() {
         var $animated = $('#content, a.contact');
         $animated.addClass('visible')
         // wait for animation to finish
