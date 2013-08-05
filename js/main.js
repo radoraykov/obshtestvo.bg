9580678558b5aa3faebe12d0463e11f389928439
Anton Stoychev <antitoxic@gmail.com>
2013-08-05 19:12:21 +0100

    Added a loader for the large image.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(function() {
+    var $veil = $('#veil')
+    new Spinner({
+        top: 'invalid',
+        left: 'invalid',
+        lines: 15, // The number of lines to draw
+        length: 0, // The length of each line
+        width: 5, // The line thickness
+        radius: 4, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        color: '#546574', // #rgb or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 31, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: true // Whether to use hardware acceleration
+    }).spin($veil.get(0));
     $('<img/>').attr('src', 'img/bg.png').load(function() {
+        $veil.fadeOut(300)
         var $animated = $('#content, a.contact');
         $animated.addClass('visible')
         // wait for animation to finish
