eccebd0a631eda68f87ef1220f0fbed19d964908
Anton Stoychev <antitoxic@gmail.com>
2013-11-22 15:55:03 +0200

    Basic slider functionality

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,51 @@
 $(function() {
-    // Тук JavaScript-а специфичен за проекта
+    var $home =  $(".home");
+    if ($home.length) {
+        initHome();
+    }
 });
+
+function initHome() {
+    var $slider = $(".slider");
+    var $slides = $slider.find('.item')
+    var slidesCount = $slides.length;
+    var $prevTrigger =  $(".slider-nav .nav.prev");
+    var $nextTrigger =  $(".slider-nav .nav.next");
+
+    var slider = $slider.owlCarousel({
+        navigation : false,
+        pagination : false,
+        autoHeight : true,
+        slideSpeed : 400,
+        paginationSpeed : 400,
+        singleItem:true,
+        afterAction: function() {
+            if (this.owl.currentItem == 0) {
+                $prevTrigger.removeClass('active');
+            } else if (this.owl.currentItem == slidesCount-1) {
+                $nextTrigger.removeClass('active');
+            } else {
+                $prevTrigger.addClass('active');
+                $nextTrigger.addClass('active');
+            }
+        }
+        // "singleItem:true" is a shortcut for:
+        // items : 1,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+
+    });
+    // Custom Navigation Events
+    $prevTrigger.click(function(){
+        if ($prevTrigger.hasClass('active')) {
+            slider.trigger('owl.prev');
+        }
+    })
+    $nextTrigger.click(function(){
+        if ($nextTrigger.hasClass('active')) {
+            slider.trigger('owl.next');
+        }
+    })
+}
