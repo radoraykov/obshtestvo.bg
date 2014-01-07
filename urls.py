c269088f033065b31c4005796ba070cd0f50bb50
antitoxic <antitoxic@gmail.com>
2014-01-07 12:41:18 +0200

    Ported current html files as django controllers.

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 
-from web.views import home
+from web.views import home, wip
 
 urlpatterns = patterns('',
                        url(r'^$', home.HomeView.as_view(), name='home'),
+                       url(r'^wip\.html$', wip.WipView.as_view(), name='wip'),
 )
--- web/views/wip.py
+++ web/views/wip.py
@@ -2,9 +2,9 @@ from django.views.generic.base import View
 from restful.decorators import restful_view_templates
 
 
-@restful_view_templates('home')
-class HomeView(View):
+@restful_view_templates('wip')
+class WipView(View):
     def get(self, request):
         return {
-            "page": "home",
+            "page": "wip",
         }
