74ebca705026f1d1d44d5bdf93f1f5a0fcbe651c
Dimitar Dimitrov <me@ddimitrov.name>
2014-04-06 00:32:36 +0300

    WIP on a deployment script
    Run the following command from within your local git clone:
    
        fab deploy
    
    This should take care of everything, provided you've made the initial
    install configuration on the target server (see the readme for info).
    
    Our app uses virtualenvwrapper and we use it on the production server,
    too.

---
+++ fabfile.py
@@ -0,0 +1,47 @@
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+env.hosts            = ['obshtestvo@tmp.obshtestvo.bg']
+env.project_root     = '/home/obshtestvo/obshtestvo.bg'
+env.repository_url   = 'https://github.com/obshtestvo/obshtestvo.bg.git'
+env.virtual_env_name = 'obshtestvobg' # leave blank if you don't use virtualenvwrapper
+
+def run_within_virtual_env(command):
+    with cd(env.project_root):
+        if env.virtual_env_name != '':
+            command_prefix = 'workon %s && ' % env.virtual_env_name
+        else:
+            command_prefix = ''
+
+        run(command_prefix + command)
+
+def run_manage(command):
+    run_within_virtual_env('python manage.py %s' % command)
+
+def deploy_static_files():
+    run_manage('collectstatic -v0 --noinput')
+
+def deploy_code():
+    with settings(warn_only=True):
+        if run('test -d %s' % env.project_root).failed:
+            run('git clone %s %s' % (env.repository_url, env.project_root))
+    with cd(env.project_root):
+        run('git pull')
+
+def install_dependencies():
+    run_within_virtual_env('pip install -r requirements.txt')
+
+def run_migrations():
+    run_manage('syncdb')
+    run_manage('migrate')
+
+def restart_app_server():
+    with cd(env.project_root):
+        run('touch web.wsgi')
+
+def deploy():
+    deploy_code()
+    install_dependencies()
+    deploy_static_files()
+    run_migrations()
+    restart_app_server()
