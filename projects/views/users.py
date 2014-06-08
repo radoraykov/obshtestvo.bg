b5ba7101decf69ab5ed46a2be4bdb8fbfdb1024e
antitoxic <antitoxic@gmail.com>
2014-06-08 08:26:39 +0300

    More descriptive example for the new page

--- projects/views/users.py
+++ projects/views/users.py
@@ -2,8 +2,26 @@ from django.views.generic.base import View
 
 from restful.decorators import restful_view_templates
 
+from projects.models import SkillGroup
+
 @restful_view_templates
 class UsersView(View):
 
     def get(self, request):
-        pass
+        skills_options = []
+        for sgroup in SkillGroup.objects.select_related('skills').all():
+            skills_options.append({
+                "text": sgroup.name,
+                "id": -1,
+                "group": sgroup.name,
+            })
+            for skill in sgroup.skills.all():
+                skills_options.append({
+                    "text": skill.name,
+                    "id": skill.id,
+                    "group": sgroup.name,
+                })
+
+        return {
+            'skills_options': skills_options
+        }
